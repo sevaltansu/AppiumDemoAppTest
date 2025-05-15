9914900e9790315d3a4d30f88826e538cd3bc825
sevaltansu <tansuseval6@gmail.com>
2025-05-15 16:44:19 +0300

    pytest fixture eklendi

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,14 +3,16 @@ from appium import webdriver
 from appium.options.android import UiAutomator2Options
 import pytest
 
-@pytest.fixture(scope="module")
+@pytest.fixture(scope="function")
 def driver():
     options = UiAutomator2Options()
     options.platform_name = "Android"
-    options.platform_version = "14"
+    options.platform_version = "11"
     options.device_name = "emulator-5554"
     options.app = "C:\\Users\\huawei\\Downloads\\mda-2.2.0-25.apk"
     options.automation_name = "UiAutomator2"
+    options.auto_grant_permissions = True
+    options.app_wait_activity = "*.*"
 
 
     driver = webdriver.Remote("http://127.0.0.1:4723", options=options)
