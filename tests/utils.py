420256ca2edf745b358522b852cba11a1c486681
sevaltansu <tansuseval6@gmail.com>
2025-05-15 16:46:18 +0300

    utils.py

--- tests/utils.py
+++ tests/utils.py
@@ -8,3 +8,13 @@ def login(driver):
     driver.find_element(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/passwordET").send_keys("10203040")
     driver.find_element(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/loginBtn").click()
     time.sleep(2)
+def add_cart(driver):
+    first_product = driver.find_element(AppiumBy.XPATH,
+                                        '//androidx.recyclerview.widget.RecyclerView[@content-desc="Displays all products of catalog"]/android.view.ViewGroup[1]')
+    first_product.click()
+    add_cart_btn = driver.find_element(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/cartBt")
+    add_cart_btn.click()
+    time.sleep(2)
+    driver.find_element(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/cartIV").click()
+    time.sleep(2)
+    driver.find_element(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/cartBt").click()
